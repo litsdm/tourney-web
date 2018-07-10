c36cfb323f32577265d8051c49af58f7fd637cd8
cdiezmoran <cdiezmoran@gmail.com>
2018-07-10 18:13:56 -0500

    Add team styles

--- src/components/SideBar/TeamNav.js
+++ src/components/SideBar/TeamNav.js
@@ -7,6 +7,16 @@ const TeamNav = () => (
   <div className={styles.TeamNav}>
     <div className={sharedStyles.Divider} />
     <div className={styles.ScrollWrap}>
+      <button
+        type="button"
+        className={styles.Team}
+        style={{ backgroundImage: 'url(https://i.pinimg.com/originals/e2/46/1f/e2461f6934b97190b0ac239e6ce6b807.png)' }}
+      />
+      <button
+        type="button"
+        className={styles.Team}
+        style={{ backgroundImage: 'url(https://i.ytimg.com/vi/QdcFvok7b8Y/maxresdefault.jpg)' }}
+      />
       <button type="button" className={styles.Add}>
         <FontAwesomeIcon icon="plus" />
       </button>
